690236d74c1f7edec246d62824cc15ec74fdae2c
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-08-21 12:01:23 +0530

    Added typedefs and init funtion for ds1302

--- include/ds1302.h
+++ include/ds1302.h
@@ -1,5 +1,20 @@
 #ifndef _DS1302_H
 #define _DS1302_H
+#include "main.h"
 
+typedef struct{
+    GPIO_TypeDef* port;
+    uint16_t pin;
+}GpioPin;
+
+
+typedef struct{
+    GpioPin CE_Pin;
+    GpioPin IO_Pin;
+    GpioPin SCLK_Pin;
+}DS1302_HandelTypeDef;
+
+
+void ds1302_init(DS1302_HandelTypeDef* handel);
 
 #endif
--- src/ds1302.c
+++ src/ds1302.c
@@ -1,2 +1,23 @@
 #include "main.h"
 #include "ds1302.h"
+
+void ds1302_init(DS1302_HandelTypeDef* handel){
+    HAL_GPIO_WritePin(handel->IO_Pin.port, handel->IO_Pin.pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(handel->SCLK_Pin.port, handel->SCLK_Pin.pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(handel->CE_Pin.port, handel->CE_Pin.pin, GPIO_PIN_RESET);
+
+    GPIO_InitTypeDef gpioinit = {
+        .Pin = handel->IO_Pin.pin,
+        .Mode = GPIO_MODE_OUTPUT_PP,
+        .Pull = GPIO_NOPULL,
+        .Speed = GPIO_SPEED_FREQ_LOW
+    };
+
+    HAL_GPIO_Init(handel->IO_Pin.port, &gpioinit);
+    
+    gpioinit.Pin = handel->CE_Pin.pin;
+    HAL_GPIO_Init(handel->CE_Pin.port, &gpioinit);
+
+    gpioinit.Pin = handel->SCLK_Pin.pin;
+    HAL_GPIO_Init(handel->SCLK_Pin.port, &gpioinit);
+}
